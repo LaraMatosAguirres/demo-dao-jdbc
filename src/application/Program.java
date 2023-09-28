d011ecac63c164e80fbb2c4499f1df7e03201607
Lara Matos Aguirres <lara.laramatos@gmail.com>
2023-09-28 09:12:50 -0300

    Criação da classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,21 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		
+		
 		Department dp = new Department(1, "Books");
-		System.out.println(dp);
+		Seller sl = new Seller(1, "Carl", "carl@gmail.com", new Date(), 3000.0, dp);
+		System.out.println(sl);
+		
+		
 
 	}
 
