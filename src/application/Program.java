c6a85197fe78b1a86dc8056aacb3ea1e3c799b6d
Lara Matos Aguirres <lara.laramatos@gmail.com>
2023-09-28 15:46:00 -0300

    Busca por departamento

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,6 @@
 package application;
 
-import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -17,7 +17,12 @@ public class Program {
 		Seller seller = sellerdao.findById(3);
 		System.out.println(seller);
 		
-		
+		System.out.println("\n=== Test 1: Seller findByDepartment ===");
+		Department dep = new Department(2, null);
+		List<Seller> list = sellerdao.findDepartment(dep);
+		for(Seller sel : list) {
+			System.out.println(sel);
+		}
 
 	}
 
